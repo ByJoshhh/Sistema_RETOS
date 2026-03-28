43e57ec7f495260c36054db48be1045fb2a2b765
ByJosh_ <byjoshmcpeyt456@gmail.com>
2026-03-28 14:51:57 -0600

    fix routes.js

--- backend_retos/routes/acarreosRoutes.js
+++ backend_retos/routes/acarreosRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const acarreosController = require('../controllers/acarreosController');
-const verificarToken = require('../middlewares/authMiddleware'); // Importamos el middleware
+const verificarToken = require('../middleware/authMiddleware'); // Importamos el middleware
 
 // Ruta POST protegida: primero pasa por verificarToken, luego va a registrarAcarreo
 router.post('/acarreos', verificarToken, acarreosController.registrarAcarreo);
